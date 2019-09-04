7e66da81e2cb8be88c147837933a293ea8d4bcc3
Alexander Kiselyov <diver.ru@gmail.com>
2019-09-04 13:46:34 +0300

    fix urls.py

--- trivio_backend/trivio_backend/urls.py
+++ trivio_backend/trivio_backend/urls.py
@@ -4,7 +4,6 @@ from rest_framework_simplejwt import views as jwt_views
 from trivio_backend.core.views import auth, posts
 
 urlpatterns = [
-    path('api/v1/auth/enrich/', auth.enrich),
     path('api/v1/auth/signup/', auth.auth_signup),
     path('api/v1/auth/login/', jwt_views.TokenObtainPairView.as_view()),
     path('api/v1/auth/refresh/', jwt_views.TokenRefreshView.as_view()),
