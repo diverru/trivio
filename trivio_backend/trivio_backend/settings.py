15c363681c9af8ee4a72fee0d2ef4c65f5383b04
Alexander Kiselyov <diver.ru@gmail.com>
2019-09-04 20:29:28 +0300

    save db at ./var/db i totally forgot that gunicorn requires static data to be served by other web server, so i'll remove it for the sake of simplicity

--- trivio_backend/trivio_backend/settings.py
+++ trivio_backend/trivio_backend/settings.py
@@ -84,7 +84,7 @@ AUTH_USER_MODEL = 'core.User'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': '/var/db/db.sqlite3',
     }
 }
 
